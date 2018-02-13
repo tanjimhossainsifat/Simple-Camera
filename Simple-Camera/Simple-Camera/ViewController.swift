e5fa41431ddb8f42aaa5c03c1eb2ab451db021ac
Tanjim Hossain <tanjim@revesoft.com>
2018-02-13 10:51:15 +0600

    Initial project in Swift

---
+++ Simple-Camera/Simple-Camera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simple-Camera
+//
+//  Created by Tanjim Hossain on 13/2/18.
+//  Copyright © 2018 Tanjim Hossain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
